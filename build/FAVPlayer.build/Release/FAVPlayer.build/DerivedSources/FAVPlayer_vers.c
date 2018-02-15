bb18c3091b2fd96e5833915cc70bd344ebb9afc0
clementperez <clement@frequency.com>
2018-02-15 15:07:28 -0800

    adds generated docs

---
+++ build/FAVPlayer.build/Release/FAVPlayer.build/DerivedSources/FAVPlayer_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char FAVPlayerVersionString[];
+ extern const double FAVPlayerVersionNumber;
+
+ const unsigned char FAVPlayerVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:FAVPlayer  PROJECT:FAVPlayer-1" "\n";
+ const double FAVPlayerVersionNumber __attribute__ ((used)) = (double)1.;
